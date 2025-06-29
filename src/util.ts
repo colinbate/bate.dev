9614736735cbd10a1f1ccfb374574eb86e56d7d5
Colin Bate <colin@colinbate.com>
2025-06-29 19:53:59 -0300

    Add garden to homepage.

--- src/util.ts
+++ src/util.ts
@@ -2,7 +2,16 @@ import type { CollectionEntry } from "astro:content";
 
 export function sortByDate(
   a: CollectionEntry<"posts">,
-  b: CollectionEntry<"posts">
+  b: CollectionEntry<"posts">,
 ) {
   return b.data.pubDate.getTime() - a.data.pubDate.getTime();
+}
+
+export function sortGardenByDate(
+  a: CollectionEntry<"garden">,
+  b: CollectionEntry<"garden">,
+) {
+  const aDate = a.data.updated || a.data.created;
+  const bDate = b.data.updated || b.data.created;
+  return bDate.getTime() - aDate.getTime();
 }
